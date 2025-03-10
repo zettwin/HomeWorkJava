6b2494590fdf748e91b73e67248fa04f3f593419
Zettwin <zettwin@gmail.com>
2025-03-10 14:53:55 +0300

    Добавлены два класса MyException, MaybeError

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            MaybeError.doIt();
+        } catch (MyException e) {
+            System.out.println("О нет, неожиданная ошибка: " + e.getMessage());
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
     }
 }
---
+++ src/main/java/MaybeError.java
@@ -0,0 +1,5 @@
+public class MaybeError {
+    public static void doIt () throws MyException{
+        throw new MyException("Что-то пошло не так! Возможно...");
+    }
+}
